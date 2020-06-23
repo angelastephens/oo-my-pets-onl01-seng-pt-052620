65d18784af0423ead97f420322f9423464c7826f
Angela Stephens <angelastephens317@gmail.com>
2020-06-23 01:49:01 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,12 +1,13 @@
 class Cat
   
-  attr_reader :name 
+  attr_reader :name, :mood
   attr_accessor :owner
   
 
   def initialize(name,owner)
     @name = name
     @owner= owner
+    @mood = "nervous"
   end
 
   

2326a3042f8ac961faa3d968c2145609f94a97fd
Angela Stephens <angelastephens317@gmail.com>
2020-06-23 01:38:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,17 +1,24 @@
 require 'pry'
 
 class Owner
-
+ 
+   @@all=[]
+   
   attr_accessor :owner
   attr_reader :name, :species
   
   def initialize(name)
     @name=name
     @species="human"
+    @@all << self
   end 
  
   def say_species
    "I am a human."
+  end
+  
+  def self.all 
+    @@all 
   end
   
 end
